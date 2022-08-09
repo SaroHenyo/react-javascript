278be60783a7ec4d0a8b50c9f7f5260dc1635e6b
SaroHenyo <cryptohenyo@gmail.com>
2022-08-09 21:05:42 +0800

    Initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { BrowserRouter } from "react-router-dom";
+import Navbar from ',/components/Navbar'
+
+function Navbar() {
+      return (
+            <BrowserRouter>
+                  <Navbar />
+            </BrowserRouter>
+      )
+}
